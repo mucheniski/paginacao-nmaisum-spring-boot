47df38ba07926ab5b694c189d73b7042ed60beb5
Diego Mucheniski <mucheniski@gmail.com>
2021-12-21 13:31:54 -0300

    Primeiro commit projeto criado

---
+++ src/main/java/com/example/paginacaonmaisumspringboot/PaginacaoNmaisumSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.paginacaonmaisumspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginacaoNmaisumSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginacaoNmaisumSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paginacaonmaisumspringboot/PaginacaoNmaisumSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paginacaonmaisumspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginacaoNmaisumSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
